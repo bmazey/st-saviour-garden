4f0c5d12b94bfc0e6275f3235bcf65ce3e95adac
brandon <b.mazey@nyu.edu>
2026-02-18 12:24:46 -0500

    updating example

---
+++ garden.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == '__main__':
+
+    # print without a line break using this function
+    print('🌷', end='')
--- silk.py
+++
@@ -1,6 +0,0 @@
-
-
-if __name__ == '__main__':
-
-    # you can print anything without a line break by using this print function
-    print('🌺', end='')
