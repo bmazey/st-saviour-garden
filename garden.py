25003fbe2ce4e052389a21935c97de03360def48
loriann <loriann952@gmail.com>
2026-03-13 23:49:41 -0400

    finished project

--- garden.py
+++ garden.py
@@ -4,3 +4,38 @@ if __name__ == '__main__':
 
     # print without a line break using this function
     print('🌷', end='')
+    plant = "🌸"
+
+    print("SOLID PATTERN")
+    for i in range(11): 
+        for j in range (11):
+            print(plant, end= "")
+        print('')
+
+    print("\nHoRIZONTAL PATTERN")
+    for i in range (11):
+        for j in range (11):
+            if i % 2 == 0:
+                print(plant, end="")
+            else:
+                print('🌻', end="")
+        print('')
+
+    print("\VERTICAL PATTERN")
+    for i in range (11):
+        for j in range (11):
+            if j % 2 == 0: 
+                print(plant, end="")
+            else: 
+                print("🌺", end="")
+        print()
+
+    print('diagonal')
+    for i in range (11):
+        for j in range (11): 
+            if i % 2 == 0 and j % 2 == 0:
+                print (plant ,end="") 
+            elif i % 2 == 1 and j % 2 == 1:
+                print(plant , end= "")
+            else:print ("🍀",end="")
+                 
